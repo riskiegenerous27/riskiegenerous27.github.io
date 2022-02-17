8e8ffda4c1cb94f555bf1f48a736e7ce61934c92
riskiegenerous27 <riskieyasrul@gmail.com>
2022-02-17 17:29:20 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".produk-populer", {
+	slidesPerView: 2,
+	grid: {
+	rows: 1,
+},
+spaceBetween: 3,
+pagination: {
+	el: ".swiper-pagination",
+	clickable: true,
+},
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+UI Interface by : 
+Figma link : https://www.figma.com/file/kSJc1jkSmdlCQmrae2gJnH/Freshly-Delivered-App-(Community)?node-id=292%3A558
